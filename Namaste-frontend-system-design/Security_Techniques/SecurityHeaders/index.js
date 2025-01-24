ba4984cf0691fba24fd3877d6443bb3c329b0a53
Pradeep Maurya <aradshub93@gmail.com>
2025-01-24 10:58:33 +0530

    updated security headers

--- Namaste-frontend-system-design/Security_Techniques/SecurityHeaders/index.js
+++ Namaste-frontend-system-design/Security_Techniques/SecurityHeaders/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
+const redirectToHttps = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    // Redirect to HTTPS
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+};
+
+app.use(redirectToHttps);
+
 // In response headers, there is 'x-powered-by: Express' , we don't want to show the server and attackers willn't be aware what kind of server is being used.
 
 app.use((req, res, next) => {
+  res.setHeader('Referrer-Policy', 'no-referrer');
   res.removeHeader('X-Powered-By');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains; preload'
+  );
   next();
 });
 
